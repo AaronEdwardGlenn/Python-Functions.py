30eb9948b50a26be9b03099f5c87c451d57f34c8
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-22 17:55:32 -0700

    Adding information on adding conditional arguments with dictionary items in functions as arguments as well as spreading arguments using * and kwargs using **

--- python_functions.py
+++ python_functions.py
@@ -21,11 +21,51 @@ def hello_function():
 
 print(hello_function().upper())
 
-# adding perameters to functions
+# adding arguments to functions
 
 
 def hello_function(greeting):
     return '{} Function.'.format(greeting)
 
 
-print(hello_function())
+print(hello_function('Hi'))
+
+# adding defaults to be added that will show up when the argument is missing. in this case, name is the default. we can add it or not as a second argument in the function
+
+
+def hello_function(greeting, name='Aaron'):
+    return '{}, {}'.format(greeting, name)
+
+
+print(hello_function('Hi', name='Edward'))
+
+# NOTE these methods must be in order or you will get an error
+
+# lets go over * and ** as arguments of our functions. this allows us to accept an abritrary number of positional or keyword arguments
+
+
+def student_info(*args, **kwargs):
+    print(args)  # these could be classes a student is taking
+    print(kwargs)   # these could be additional facts about students
+
+
+student_info('Math', 'Art', faveColor='green', faveClass='psy')
+# NOTE positional args printed out here as a Tuple and our kwargs are a dictionary with keyword values.
+
+# so here we are spreading arguments in a way that allows them to be accessed as a list or as a dictionary
+
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+courses = ('Math', 'Art')
+info = {'faveColor': 'green', 'faveClass': 'psy'}
+
+# adding them in like this gives us the entire list and dictionary. we just want what is inside of these things
+student_info(courses, info)
+
+student_info(*courses, **info)  # this gives us our individual list and dictionary
+
+# NOTE conditional args vs kwargs is something to review for later
